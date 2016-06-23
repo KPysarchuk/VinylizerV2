332dbeaaec64305bc5db9c492f33050ce466d487
Kostya <kot2206@gmail.com>
2016-06-23 16:55:40 +0300

    Initial commit

--- Vinylizer/Controllers/HomeController.cs
+++ Vinylizer/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using NAudio.Wave;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vinylizer.Models;
 
 namespace Vinylizer.Controllers
 {
@@ -13,18 +16,47 @@ namespace Vinylizer.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult GetAudioFileForPlay(string fileName)
         {
-            ViewBag.Message = "Your application description page.";
+            byte[] fileBytes = System.IO.File.ReadAllBytes(string.Format("~App_Data/{0}", fileName));
+            MemoryStream ms = new MemoryStream(fileBytes);
 
-            return View();
+            return File(ms, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
         }
 
-        public ActionResult Contact()
+        public ActionResult GetFilterForPlay(int filterId)
         {
-            ViewBag.Message = "Your contact page.";
+            string fileName = filterId.ToString() + ".mp3";
+            byte[] fileBytes = System.IO.File.ReadAllBytes("~App_Data/" + fileName);
+            MemoryStream ms = new MemoryStream(fileBytes);
 
-            return View();
+            return File(ms, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+        }
+
+        public ActionResult GetAudioFileForDownload(string fileName, FilterModel[] filters)
+        {
+            int usedFilters = 1;
+            Mp3FileReader reader = new Mp3FileReader("~App_Data/" + fileName);
+            TimeSpan duration = reader.TotalTime;
+            string mergeString = "-i " + fileName;
+
+            foreach (var filter in filters)
+            {
+                if (filter.Volume != 0)
+                {
+                    string filterName = Converter.Loop(filter.Id, duration);
+                    string finishedName = Converter.ChangeVolume(filter.Volume, fileName);
+                    mergeString = mergeString + " -i " + finishedName;
+                    usedFilters++;
+                }
+            }
+
+            Converter.Merge(mergeString, fileName, usedFilters);
+
+            string mixName = string.Format("Converted{0}", fileName);
+            byte[] fileBytes = System.IO.File.ReadAllBytes(string.Format("~/App_Data/{0}", mixName));
+
+            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, mixName);
         }
     }
 }
---
+++ Vinylizer/Models/Converter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vinylizer.Models
+{
+    public class Converter
+    {
+        public static void Merge(string mergeString, string fileName, int usedFilters)
+        {
+            string output = string.Format("Converted{0}/", fileName);
+            string command = string.Format("{0} -filter_complex amix=inputs={1}:duration=first:dropout_transition=2 {2}", mergeString, usedFilters, fileName);
+            var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+            ffMpeg.Invoke(command);
+        }
+
+        public static string ChangeVolume(int volumePart, string filterName)
+        {
+            Random rnd = new Random();
+            string output = string.Format("filter{0}.mp3", rnd.Next(1000000000));
+            string command = string.Format(@"-i {0} -af ""volume={1}"" {2}", filterName, volumePart, output).Replace("\"", string.Empty);
+            var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+            ffMpeg.Invoke(command);
+
+            return output;
+        }
+
+        public static string Loop(int filterId, TimeSpan duration)
+        {
+            Random rnd = new Random();
+            string file = string.Format("loop{0}.txt", filterId.ToString());
+            string output = string.Format("filter{0}.mp3", rnd.Next(1000000000));
+            string command = string.Format("- t {0} - f concat - i {1} - c copy - t {0} {2}", duration.TotalSeconds.ToString(), file, output);
+            var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+            ffMpeg.Invoke(command);
+
+            return output;
+        }
+    }
+}
